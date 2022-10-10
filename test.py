e972ebfe5d9db410c88fe5a97318835d90e679e3
JamessenLiu <461614816@qq.com>
2022-10-10 14:25:55 +0800

    add sum func

--- test.py
+++ test.py
@@ -13,3 +13,15 @@ def fib(n):
 
 f = fib(999)
 print(sys.getsizeof(f))
+
+
+# master / main 主分支  -> 生产环境
+# staging / testing 测试分支  -> 测试环境
+# develop / dev 开发分支  -> 开发环境
+
+# feat/send_email  / feat/num-1 -> 需求分支命名
+# fix/email_validation_error -> 修复问题分支命名
+# improve/XXX -> 优化分支命名
+
+def get_sum(a, b):
+    return a + b
