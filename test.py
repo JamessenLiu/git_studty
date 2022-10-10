c430cf30e8cb152b06d180cbf6d788b58164d019
JamessenLiu <461614816@qq.com>
2022-10-10 14:00:37 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+
+import sys
+
+
+def fib(n):
+
+    a, b, counter = 0, 1, 0
+    while counter < n:
+        yield a
+        a, b = b, a + b
+        counter += 1
+
+
+f = fib(999)
+print(sys.getsizeof(f))
